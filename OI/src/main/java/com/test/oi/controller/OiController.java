3c56b9d00d4389529a0d32b5d8cd535867480b1b
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-22 13:53:41 +0630

    controller update

--- OI/src/main/java/com/test/oi/controller/OiController.java
+++ OI/src/main/java/com/test/oi/controller/OiController.java
@@ -4,14 +4,12 @@ import com.test.oi.dto.PaymentRequest;
 import com.test.oi.dto.TransactionRequest;
 import com.test.oi.service.Oiservice;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/oi/api/v1")
@@ -25,19 +23,16 @@ public class OiController {
     @PostMapping("/request")
     public ResponseEntity<String> request(@RequestBody TransactionRequest request) {
 
-        String targetUrl = "http://localhost:8080/something,something";
+        String targetUrl = "http://localhost:8080/target-endpoint";
 
-        // Set headers
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // Wrap the request
         HttpEntity<TransactionRequest> entity = new HttpEntity<>(request, headers);
 
-        // Make the POST request to the external API
-        ResponseEntity<String> response = restTemplate.postForEntity(targetUrl, entity, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                targetUrl, entity, String.class);
 
-        // Return the external API response
         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
 
