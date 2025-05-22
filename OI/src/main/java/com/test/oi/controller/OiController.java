ddfc2f825a2aa6f6d598e3501aca937e0a59d212
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-22 11:21:32 +0630

    second commit

--- OI/src/main/java/com/test/oi/controller/OiController.java
+++ OI/src/main/java/com/test/oi/controller/OiController.java
@@ -1,16 +1,19 @@
 package com.test.oi.controller;
 
+import com.test.oi.service.Oiservice;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/oi/api/v1")
+@RequiredArgsConstructor
 public class OiController {
 
-    @GetMapping("")
-    public ResponseEntity<String> getOi() {
-        return ResponseEntity.ok("");
+    private final Oiservice Oiservice;
+
+    @PostMapping("/{id}/{amount}")
+    public ResponseEntity<String> Pay(@PathVariable Long id,@PathVariable String amount) {
+        return ResponseEntity.ok(Oiservice.pay(id, amount));
     }
 }
